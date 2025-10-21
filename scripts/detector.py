7e2024248c6257edd601ef691f4bca30e4431783
Jakob Thumm <thumm@stanford.edu>
2025-10-21 14:40:20 -0700

    Hacked the bottle and cup labels for now.

--- scripts/detector.py
+++ scripts/detector.py
@@ -16,13 +16,15 @@ from torchvision import transforms
 from torchvision.models.detection import ssdlite320_mobilenet_v3_large
 from torchvision.utils import draw_bounding_boxes
 
+# I cannot find a source for these!
+# It is very important that these are aligned with the finetuning step.
 COCO_LABELS = [
     '__background__', 'person', 'bicycle', 'car', 'motorcycle', 'airplane', 'bus',
     'train', 'truck', 'boat', 'traffic light', 'fire hydrant', 'N/A', 'stop sign',
     'parking meter', 'bench', 'bird', 'cat', 'dog', 'horse', 'sheep', 'cow',
     'elephant', 'bear', 'zebra', 'giraffe', 'N/A', 'backpack', 'umbrella', 'N/A', 'N/A',
     'handbag', 'tie', 'suitcase', 'frisbee', 'skis', 'snowboard', 'sports ball',
-    'kite', 'baseball bat', 'baseball glove', 'skateboard', 'surfboard', 'tennis racket',
+    'kite', 'baseball bat', 'bottle', 'skateboard', 'cup', 'tennis racket',
     'bottle', 'N/A', 'wine glass', 'cup', 'fork', 'knife', 'spoon', 'bowl',
     'banana', 'apple', 'sandwich', 'orange', 'broccoli', 'carrot', 'hot dog', 'pizza',
     'donut', 'cake', 'chair', 'couch', 'potted plant', 'bed', 'N/A', 'dining table',
